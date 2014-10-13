879e0a0bb375f56cc3cc5ade6be227a5bcda1862
seppel1992 <felixkrautschuk@yahoo.de>
2014-10-13 15:03:21 +0200

    NavigationDrawer wird korrekt angezeigt und geschlossen

--- NavigationDrawerTutorial3_Icon/app/src/main/java/com/example/felix/navigationdrawertutorial3_icon/MainActivity.java
+++ NavigationDrawerTutorial3_Icon/app/src/main/java/com/example/felix/navigationdrawertutorial3_icon/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.felix.navigationdrawertutorial3_icon;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
@@ -14,12 +18,14 @@ public class MainActivity extends Activity
     private DrawerLayout drawerLayout;
     private ListView drawerList;
     private ObjectDrawerItem[] objectDrawerItems;
+    DrawerItemCustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("NavigationDrawer");
 
         navigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -33,6 +39,11 @@ public class MainActivity extends Activity
         objectDrawerItems[5] = new ObjectDrawerItem(R.drawable.apple, "Apple");
         objectDrawerItems[6] = new ObjectDrawerItem(R.drawable.videos, "Videos");
         objectDrawerItems[7] = new ObjectDrawerItem(R.drawable.contact, "Contact Us");
+
+        adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, objectDrawerItems);
+        drawerList.setAdapter(adapter);
+
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
 
@@ -52,5 +63,35 @@ public class MainActivity extends Activity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener
+    {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            selectItem(position);
+        }
+
+        private void selectItem(int position)
+        {
+            Fragment fragment = null;
+
+            switch (position)
+            {
+
+            }
+
+            if (fragment != null)
+            {
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                drawerList.setItemChecked(position, true);
+                drawerList.setSelection(position);
+                getActionBar().setTitle(navigationDrawerItemTitles[position]);
+                drawerLayout.closeDrawer(drawerList);
+            }
+        }
     }
 }
